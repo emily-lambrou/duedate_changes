db9f38970ce5810e31e5ca07f0b9586369264067
emily-lambrou <emily-l93@hotmail.com>
2024-09-03 13:10:15 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -19,15 +19,3 @@ def prepare_missing_fields_comment(issue: dict, assignees: dict):
 
     return comment
 
-def check_comment_exists(issue_id, comment_text):
-    """Check if the comment already exists on the issue."""
-    comments = graphql.get_issue_comments(issue_id)
-    for comment in comments:
-        if comment_text in comment.get('body', ''):
-            return True
-    return False
-
-
-
-
-
