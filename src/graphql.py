e800e3e11ccbb1108da6748e3abe16f12eebd30e
emily-lambrou <emily-l93@hotmail.com>
2024-09-03 13:05:14 +0300

    Create graphql.py

---
+++ src/graphql.py
@@ -0,0 +1,260 @@
+from pprint import pprint
+import requests
+import logging
+import config
+import utils
+
+def get_repo_issues(owner, repository, duedate_field_name, after=None, issues=None):
+    query = """
+    query GetRepoIssues($owner: String!, $repo: String!, $duedate: String!, $after: String) {
+          repository(owner: $owner, name: $repo) {
+            issues(first: 100, after: $after, states: [OPEN]) {
+              nodes {
+                id
+                title
+                number
+                url
+                assignees(first:100) {
+                  nodes {
+                    name
+                    email
+                    login
+                  }
+                }
+                projectItems(first: 10) {
+                  nodes {
+                    project {
+                      number
+                      title
+                    }
+                    fieldValueByName(name: $duedate) {
+                      ... on ProjectV2ItemFieldDateValue {
+                        id
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+                hasPreviousPage
+              }
+              totalCount
+            }
+          }
+        }
+    """
+
+    variables = {
+        'owner': owner,
+        'repo': repository,
+        'duedate': duedate_field_name,
+        'after': after
+    }
+
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": query, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+
+    if response.json().get('errors'):
+        print(response.json().get('errors'))
+
+    pageinfo = response.json().get('data').get('repository').get('issues').get('pageInfo')
+    if issues is None:
+        issues = []
+    issues = issues + response.json().get('data').get('repository').get('issues').get('nodes')
+    if pageinfo.get('hasNextPage'):
+        return get_repo_issues(
+            owner=owner,
+            repository=repository,
+            after=pageinfo.get('endCursor'),
+            issues=issues,
+            duedate_field_name=duedate_field_name
+        )
+
+    return issues
+
+def get_project_issues(owner, owner_type, project_number, duedate_field_name, filters=None, after=None, issues=None):
+    query = f"""
+    query GetProjectIssues($owner: String!, $projectNumber: Int!, $duedate: String!, $after: String)  {{
+          {owner_type}(login: $owner) {{
+            projectV2(number: $projectNumber) {{
+              id
+              title
+              number
+              items(first: 100,after: $after) {{
+                nodes {{
+                  id
+                  fieldValueByName(name: $duedate) {{
+                    ... on ProjectV2ItemFieldDateValue {{
+                      id
+                      date
+                    }}
+                  }}
+                  content {{
+                    ... on Issue {{
+                      id
+                      title
+                      number
+                      state
+                      url
+                      assignees(first:20) {{
+                        nodes {{
+                          name
+                          email
+                          login
+                        }}
+                      }}
+                    }}
+                  }}
+                }}
+                pageInfo {{
+                endCursor
+                hasNextPage
+                hasPreviousPage
+              }}
+              totalCount
+              }}
+            }}
+          }}
+        }}
+    """
+
+    variables = {
+        'owner': owner,
+        'projectNumber': project_number,
+        'duedate': duedate_field_name,
+        'after': after
+    }
+
+    response = requests.post(
+        config.api_endpoint,
+        json={"query": query, "variables": variables},
+        headers={"Authorization": f"Bearer {config.gh_token}"}
+    )
+
+    if response.json().get('errors'):
+        print(response.json().get('errors'))
+
+    pageinfo = response.json().get('data').get(owner_type).get('projectV2').get('items').get('pageInfo')
+    if issues is None:
+        issues = []
+
+    nodes = response.json().get('data').get(owner_type).get('projectV2').get('items').get('nodes')
+
+    if filters:
+        filtered_issues = []
+        for node in nodes:
+            if filters.get('open_only') and node['content'].get('state') != 'OPEN':
+                continue
+           
+            filtered_issues.append(node)
+
+        nodes = filtered_issues
+
+    issues = issues + nodes
+
+    if pageinfo.get('hasNextPage'):
+        return get_project_issues(
+            owner=owner,
+            owner_type=owner_type,
+            project_number=project_number,
+            after=pageinfo.get('endCursor'),
+            filters=filters,
+            issues=issues,
+            duedate_field_name=duedate_field_name
+        )
+
+    return issues
+
+
+
+def add_issue_comment(issueId, comment):
+    mutation = """
+    mutation AddIssueComment($issueId: ID!, $comment: String!) {
+        addComment(input: {subjectId: $issueId, body: $comment}) {
+            clientMutationId
+        }
+    }
+    """
+
+    variables = {
+        'issueId': issueId,
+        'comment': comment
+    }
+
+    try:
+        response = requests.post(
+            config.api_endpoint,
+            json={"query": mutation, "variables": variables},
+            headers={"Authorization": f"Bearer {config.gh_token}"}
+        )
+        data = response.json()
+
+        if 'errors' in data:
+            logging.error(f"GraphQL mutation errors: {data['errors']}")
+
+        return data.get('data')
+
+    except requests.RequestException as e:
+        logging.error(f"Request error: {e}")
+        return {}
+
+def get_issue_comments(issue_id):
+    query = """
+    query GetIssueComments($issueId: ID!) {
+        node(id: $issueId) {
+            ... on Issue {
+                comments(first: 100) {
+                    nodes {
+                        body
+                        createdAt
+                        author {
+                            login
+                        }
+                    }
+                    pageInfo {
+                        endCursor
+                        hasNextPage
+                    }
+                }
+            }
+        }
+    }
+    """
+
+    variables = {
+        'issueId': issue_id
+    }
+
+    try:
+        response = requests.post(
+            config.api_endpoint,
+            json={"query": query, "variables": variables},
+            headers={"Authorization": f"Bearer {config.gh_token}"}
+        )
+        
+        data = response.json()
+
+        if 'errors' in data:
+            logging.error(f"GraphQL query errors: {data['errors']}")
+            return []
+
+        comments_data = data.get('data', {}).get('node', {}).get('comments', {})
+        comments = comments_data.get('nodes', [])
+
+        # Handle pagination if there are more comments
+        pageinfo = comments_data.get('pageInfo', {})
+        if pageinfo.get('hasNextPage'):
+            next_page_comments = get_issue_comments(issue_id, after=pageinfo.get('endCursor'))
+            comments.extend(next_page_comments)
+
+        return comments
+
+    except requests.RequestException as e:
+        logging.error(f"Request error: {e}")
+        return []
