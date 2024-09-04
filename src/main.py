20f01bf12e10b9d475ac7f49c836631764945b69
emily-lambrou <emily-l93@hotmail.com>
2024-09-04 09:35:33 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -24,14 +24,18 @@ def notify_due_date_changes():
         issue_id = issue.get('id')
         due_date = issue.get('fieldValueByName', {}).get('date')
 
-        # Directly notify about the due date
-        comment = f"The current due date is {due_date}."
-        if not config.dry_run:
-            graphql.add_issue_comment(issue_id, comment)
+        # Get all comments on the issue
+        comments = graphql.get_all_issue_comments(issue_id)
+        expected_comment = f"The due date is updated to {due_date}."
 
-        logger.info(f'Comment added to issue with ID {issue_id}. Due date is {due_date}')
-
-
+        # Check if the due date is already mentioned in any comment
+        if any(expected_comment in comment for comment in comments):
+            logger.info(f"Due date {due_date} already mentioned in issue {issue_id}. Skipping...")
+        else:
+            # Directly notify about the due date
+            if not config.dry_run:
+                graphql.add_issue_comment(issue_id, expected_comment)
+            logger.info(f'Comment added to issue with ID {issue_id}. Due date is {due_date}')
 
 def main():
     logger.info('Process started...')
@@ -39,7 +43,7 @@ def main():
         logger.info('DRY RUN MODE ON!')
 
     notify_due_date_changes()
-   
 
 if __name__ == "__main__":
     main()
+
