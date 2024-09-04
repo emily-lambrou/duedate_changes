5d1e277e242800a750bd4be1446720b9730a3959
emily-lambrou <emily-l93@hotmail.com>
2024-09-04 10:52:18 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -19,3 +19,11 @@ def prepare_duedate_comment(issue: dict, assignees: dict, due_date):
 
     return comment
 
+
+def check_comment_exists(issue_id, comment_text):
+    """Check if the comment already exists on the issue."""
+    comments = graphql.get_issue_comments(issue_id)
+    for comment in comments:
+        if comment_text in comment.get('body', ''):
+            return True
+    return False
