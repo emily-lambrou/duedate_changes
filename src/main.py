1c9f926c2f38555447032e3b1e1baa934bf4ea86
emily-lambrou <emily-l93@hotmail.com>
2024-09-05 08:53:39 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from logger import logger
-from datetime import datetime, timedelta
-import requests
+from datetime import datetime
 import config
 import utils
 import graphql
@@ -34,14 +33,21 @@ def notify_due_date_changes():
         assignees = issue.get('assignees', {}).get('nodes', [])
 
         # Get the due date value
-        due_date = projectItem.get('fieldValueByName', {}).get('date')
-        # Convert it to date object
-        due_date_obj = datetime.strptime(due_date, "%Y-%m-%d").date()
-        
+        due_date = None
+        due_date_obj = None
+        try:
+            if projectItem:
+                due_date = projectItem.get('fieldValueByName', {}).get('date')
+                if due_date:
+                    due_date_obj = datetime.strptime(due_date, "%Y-%m-%d").date()
+        except (AttributeError, ValueError) as e:
+            logger.error(f"Error processing due date for issue {issue['title']}: {e}")
+            continue  # Skip this issue and move to the next
+
         issue_title = issue['title']
         issue_id = issue['id']
-     
-        if not due_date:
+
+        if not due_date_obj:
             logger.info(f'No due date found for issue {issue_title}')
             continue
         
@@ -62,7 +68,6 @@ def notify_due_date_changes():
                     graphql.add_issue_comment(issue_id, comment)    
                 logger.info(f'Comment added to issue with title {issue_title}. Due date is {due_date_obj}.')
 
-
 def main():
     logger.info('Process started...')
     if config.dry_run:
@@ -72,4 +77,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
